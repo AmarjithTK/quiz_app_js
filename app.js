253da7a7a99118fc53c576f104570c78acc42403
trinity <AmarjithTK>
2020-04-08 14:18:54 +0530

    Quizee app nearing completion
    Major changes
    
     - [x] Added SVG animation before the loading of questions
     - [x] Used localstorage to share url instead of js -- cookies
     - [x] Included fontawesome library to use icons
    
     Changelog
    
      - Project design and styling Completed
      - Automatically navigate to next question after answering
      - More features and navigation menu added
      - Added the ability to choose difficulty and number of questions
      - Montserrat is the new font now and it is used everywhere
      - Added show correct answers section
    
     Notes
    
     * Need a major refactoring of code on the next commit
     * Require Minfying of javascript code for more speed
     * Better to add Google Analytics
     * Use inline comments for future readablity

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const accesskey = localStorage.getItem('access')
 
 console.log(accesskey)
 
+const loader = document.querySelector('.loader')
+
+
+
+const RemoveLoader = () => {
+    setTimeout(()=>{
+        loader.style.display = 'none'
+    },
+    1000)
+}
 
 
 const TriggerConnection = (callback) => {
@@ -16,6 +26,8 @@ const TriggerConnection = (callback) => {
         if (connect.readyState === 4 && connect.status === 200) {
             Questiondata = JSON.parse(connect.responseText)
             callback(undefined, Questiondata)
+            console.log(Questiondata)
+            RemoveLoader()
         } else if (connect.readyState === 4) {
             callback('The connection established but no data collected', connect.responseText)
         }
@@ -47,6 +59,8 @@ TriggerConnection((err, data) => {
 
     if (err) {
         console.log(err)
+  
+        
     } else (
         generateQuestions(data, 0)
     )
@@ -56,9 +70,9 @@ TriggerConnection((err, data) => {
 
 const buttons = document.querySelectorAll('button')
 
-const question = document.querySelector('h1')
+const question = document.querySelector('h2')
 
-
+const questiontracker = document.querySelector('.question_tracker')
 
 
 
@@ -66,6 +80,7 @@ var correctAnswer
 var questionIndex
 var Questiondata
 var questions
+var ScoreKeeper = 0
 
 let QuestionKeeper = new Array()
 
@@ -110,7 +125,10 @@ const generateQuestions = (data, questionnumber) => {
 
     generateInputField(AllOptions, data)
 
-    question.innerHTML = questions
+    questiontracker.innerHTML = `Question ${questionIndex+1}&nbsp;/&nbsp;${Questiondata.results.length}`
+  
+    question.innerHTML = `${questions} `
+
 
 
 }
@@ -118,8 +136,11 @@ const generateQuestions = (data, questionnumber) => {
 
 const CheckAnswer = (GivenAnswer) => {
 
-    if (correctAnswer == GivenAnswer.innerHTML)
+    if (correctAnswer == GivenAnswer.innerHTML){
         GivenAnswer.classList.add('right_answer')
+        ScoreKeeper++
+
+    }
     else
         GivenAnswer.classList.add('wrong_answer')
 
@@ -132,11 +153,12 @@ const CheckAnswer = (GivenAnswer) => {
     console.log(QuestionKeeper,questionIndex)
 
 
-    if (questionIndex != 25) {
+    if (questionIndex != Questiondata.results.length) {
         setTimeout(() => {
 
 
-            GivenAnswer.classList.remove(GivenAnswer.classList[GivenAnswer.classList.length - 1])
+            GivenAnswer.classList.remove('wrong_answer')
+            GivenAnswer.classList.remove('right_answer')
 
             setTimeout(() => {
                 generateQuestions(Questiondata, questionIndex)
@@ -150,14 +172,134 @@ const CheckAnswer = (GivenAnswer) => {
     else{
 
 
-        // DisplayResults(QuestionKeeper)
-        document.body.innerHTML = ''
+        setTimeout(() => {
+            showResult(ScoreKeeper,QuestionKeeper)
+        }, 1000)
+
+      
 
 
     }
 
 
 }
+
+
+const showResult = (ScoreKeeper,QuestionKeeper) => {
+    
+
+const ScoreTemplate = `<div class="container" style="min-height:100vh;">
+<div class="wrapper">
+    <div class="card text-center" style="max-width: 25rem;">
+
+        <div class="card-body">
+            <h4 class="card-title text-primary my-4"  style="font-family: 'Montserrat', sans-serif; font-weight:900">You have scored</h4>
+            <h1 class="card-text text-primary my-1"  style="font-family: 'Montserrat', sans-serif; font-weight:900">${ScoreKeeper} &nbsp;/ &nbsp;${Questiondata.results.length}</h1>
+
+        </div>
+        <div class="card-body">
+            <button type="button" class="btn btn-primary mx-1 show_answer"  style="font-family: 'Montserrat', sans-serif; font-weight:900">Show Answers</button>
+            <a href="index.html"><button type="button" class="btn btn-primary mx-1"  style="font-family: 'Montserrat', sans-serif; font-weight:900">play again</button></a>
+        </div>
+    </div>
+</div>
+</div>
+
+<style>
+body{
+    background-color:#007bff;
+}
+
+.container{
+    min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
+  min-height: 100vh; /* These two lines are counted as one :-)       */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
+`
+
+document.body.innerHTML = ScoreTemplate;
+
+
+const show_answer = document.querySelector('.show_answer')
+
+show_answer.addEventListener( 'click',()=> {
+    ShowAnswer(QuestionKeeper,ScoreKeeper)
+}
+
+)
+
+}
+
+
+
+const ShowAnswer = (ScoreKeeper) => {
+
+document.body.innerHTML= `
+<nav class="navbar navbar-expand-lg navbar-light bg-primary text-light">
+
+<h3 class="text-light my-5 mx-4"  style="font-family: 'Montserrat', sans-serif; font-weight:900"> <a href="index.html"><i class="fas fa-long-arrow-alt-left mx-4"
+            style="color: white; vertical-align: middle;"></i></a> Quizee <span
+        class="badge badge-dark">Trivia</span></h2>
+
+
+</nav>`
+
+
+document.body.style.backgroundColor = '#007bff'
+    for (let index = 0; index < QuestionKeeper.length; index++) {
+        const ActualQuestion = QuestionKeeper[index].Qn;
+        const ActualAnswer = QuestionKeeper[index].CA;
+        const ActualChoice = QuestionKeeper[index].GA;
+        
+        if(ActualAnswer==ActualChoice){
+            document.body.innerHTML+= `
+
+            <div class="container my-2 justify-content-center d-flex">
+            <div class="results_content w-100" style="border-color:green;">
+            
+                <h6 class="qn_number"> Question ${index+1} </h6>
+                <h4 class="qn">${ActualQuestion}</h4>
+                <h5 class="canswer">Correct Answer &nbsp;-&nbsp ${ActualAnswer}</h5>
+               
+            </div>
+          </div
+            `
+        }
+        else{
+            document.body.innerHTML+= `
+
+            <div class="container my-2 justify-content-center d-flex">
+            <div class="results_content w-100" style="border-color:red;">
+            
+                <h6 class="qn_number"> Question ${index+1} </h6>
+                <h4 class="qn">${ActualQuestion}</h4>
+                <h5 class="ganswer">Your Answer &nbsp;-&nbsp ${ActualChoice}</h5>
+                <h5 class="canswer">Correct Answer &nbsp;-&nbsp ${ActualAnswer}</h5>
+               
+            </div>
+          </div
+            `
+
+        }
+
+      
+
+
+
+        
+    }
+
+
+
+
+
+
+}
+
 
 
 
@@ -234,54 +376,3 @@ const generateInputField = (AllOptions, data) => {
 
 
 
-// form.addEventListener('submit', event => {
-//     event.preventDefault()
-
-//     let score = 0
-
-//     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
-
-
-//     userAnswers.forEach((answer, index) => {
-
-//         if (answer == correctAnswer[index])
-//             score += 25;
-
-
-
-
-//     })
-
-//     scrollTo(0, 0)
-
-//     resultDiv.classList.remove('d-none')
-
-
-
-
-//     let output = 0;
-
-//     const timer = setInterval(() => {
-
-//         resultSpan.textContent = `${output} %`;
-
-//         if (output == score)
-//             clearInterval(timer)
-//         else {
-//             output++
-//         }
-
-//     }, 30)
-
-
-
-
-// })
-
-
-
-// set timeout
-
-setInterval(() => {
-
-}, 2000)
